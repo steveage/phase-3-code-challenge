814e35f6b3f695146e5f39946c8131e9ed857f8e
Natalie Perpepaj <perpepajn25@gmail.com>
2018-04-13 15:12:54 -0400

    Update band.rb

--- band.rb
+++ band.rb
@@ -2,10 +2,9 @@ class Band
 
   ALL = []
 
-  attr_accessor :name
-
-  def initialize(name)
+  def initialize(name, genre)
     @name = name
+    @genre = genre
     ALL << self
   end
 
@@ -18,4 +17,4 @@ end
 # DELIVERABLES
 #A band should have a method Band#plays_in_venue that takes a venue and name as a string as arguements and associates the band to that venue
 #A band shoulda have a method Band#concerts should return an array of all that band's concerts
-#A band should have a method Band#frequently_visited should return the venue that that band has played at most
+#A band should have a method Band.all_introductions that puts out a message of "Hello, we are {insert band name here}. Our genre is {insert genre}" for each band
