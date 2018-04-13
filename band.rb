a3aa0db036e0f79ab33f8b86a1386af698419715
Natalie Perpepaj <perpepajn25@gmail.com>
2018-04-13 15:15:27 -0400

    Update band.rb

--- band.rb
+++ band.rb
@@ -15,6 +15,6 @@ class Band
 end
 
 # DELIVERABLES
-#A band should have a method Band#plays_in_venue that takes a venue and name as a string as arguements and associates the band to that venue
+#A band should have a method Band#plays_in_venue that takes a venue and date as a string as arguements and associates the band to that venue
 #A band shoulda have a method Band#concerts should return an array of all that band's concerts
 #A band should have a method Band.all_introductions that puts out a message of "Hello, we are {insert band name here}. Our genre is {insert genre}" for each band
